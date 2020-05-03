9dc42c43ae44bdf6fbdec92e11b32d38e9e4dd67
Carmine Colicino <carmine@pragmatic.agency>
2020-05-03 01:20:04 +0100

    Implement custom sass function to import variables from json file

---
+++ wp-content/themes/wally/config/sass-functions.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const themeJSON = require("./theme.json");
+const sass = require("node-sass");
+
+module.exports = {
+	"get($key)": function(key) {
+		key = key.getValue();
+		const json = themeJSON;
+		const sassVars = json[key];
+
+		const map = new sass.types.Map(Object.keys(sassVars).length);
+
+		Object.entries(sassVars).forEach(([colorName, colorHex], index) => {
+			map.setKey(index, new sass.types.String(colorName));
+			map.setValue(index, new sass.types.String(colorHex));
+		});
+
+		return map;
+	}
+};
