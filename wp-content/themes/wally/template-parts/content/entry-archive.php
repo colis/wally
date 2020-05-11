8c6aa432f647b64b5b8485474841ec6932dacf9c
Carmine Colicino <carmine@pragmatic.agency>
2020-05-11 22:20:04 +0100

    Revert custom image sizes

--- wp-content/themes/wally/template-parts/content/entry-archive.php
+++ wp-content/themes/wally/template-parts/content/entry-archive.php
@@ -8,9 +8,7 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class( 'o-content-list__item' ); ?>>
-	<?php $crop_location = get_post_meta( get_the_ID(), 'crop-location', true ); ?>
-	<?php $image_size = $crop_location ? "project-archive-$crop_location" : 'project-archive'; ?>
-	<?php the_post_thumbnail( $image_size ); ?>
+	<?php the_post_thumbnail( 'project-archive' ); ?>
 	<a href="<?php the_permalink(); ?>" class="project-meta">
 		<h3 class="project-title"><?php the_title(); ?></h3>
 		<?php $categories = get_the_category(); ?>
