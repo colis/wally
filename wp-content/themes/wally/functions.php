c852c644dd2dee3115b8b7307400b519066b3267
Carmine Colicino <carmine@pragmatic.agency>
2020-05-14 19:10:16 +0100

    Bump theme version

--- wp-content/themes/wally/functions.php
+++ wp-content/themes/wally/functions.php
@@ -9,7 +9,7 @@
 
 if ( ! defined( 'THEME_VERSION' ) ) {
 	// Replace the version number of the theme on each release.
-	define( 'THEME_VERSION', '1.0.0' );
+	define( 'THEME_VERSION', '1.1.0' );
 }
 
 if ( ! function_exists( 'wally_setup' ) ) :
