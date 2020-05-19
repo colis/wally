f8258b7fa50987e2a29ea227b4501a1dcf586fc7
Carmine Colicino <carmine@pragmatic.agency>
2020-05-19 19:40:34 +0100

    Add footer

--- wp-content/themes/wally/footer.php
+++ wp-content/themes/wally/footer.php
@@ -11,6 +11,15 @@
 
 ?>
 
+<footer id="footer" class="c-footer">
+	<div class="o-container">
+		<div class="c-footer__info">
+			<p class="c-footer__copyright"><?php echo esc_html( gmdate( 'Y' ) ); ?> © Veronica Urbano • All rights reserved</p>
+			<p class="c-footer__authors">Designed by me • Developed by a lazy <a href="https://github.com/colis">programmer</a></p>
+		</div><!-- .c-footer__info -->
+	</div><!-- .o-container -->
+</footer><!-- #footer -->
+
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
