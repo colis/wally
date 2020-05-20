a25342c6d5b855f12e9f4daaa22fd6e848aa3638
Carmine Colicino <carmine@pragmatic.agency>
2020-05-20 10:07:01 +0100

    Fix menu markup

--- wp-content/themes/wally/template-parts/header/social-links.php
+++ wp-content/themes/wally/template-parts/header/social-links.php
@@ -15,9 +15,11 @@ $num_menu_items = count( $menu_items );
 
 <ul id="menu-social-networks" class="c-navigation__menu-wrapper">
 	<?php foreach ( $menu_items as $index => $menu_item ) : ?>
-		<li class="menu-item menu-item--social-networks">
+		<li class="menu-item menu-item__social-networks">
 			<a href="<?php echo esc_url( $menu_item->url ); ?>"><?php echo esc_html( $menu_item->title ); ?></a>
 		</li>
-		<?php echo $index !== $num_menu_items - 1 ? '/' : ''; ?>
+		<?php if ( $index !== $num_menu_items - 1 ) : ?>
+			<li class="menu-item menu-item__separator">/</li>
+		<?php endif; ?>
 	<?php endforeach; ?>
 </ul>
